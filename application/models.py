e0c1db4c870f860f489f701b326db9e84ad77d0a
minji <my970524@naver.com>
2022-06-21 21:24:45 +0900

    add: 지원 내역 모델링
    - application/models.py에 Application 모델 생성
    - migrate하여 DB 반영
    - admin 사이트에 Application 모델 추가

--- application/models.py
+++ application/models.py
@@ -1,3 +1,22 @@
+from turtle import ondrag
 from django.db import models
 
+from recruit.models import Advertisement
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your models here.
+class Application(models.Model):
+    applicant = models.ForeignKey(
+        to=User,
+        verbose_name='지원자',
+        on_delete=models.CASCADE,
+        related_name='applications'
+    )
+    ad = models.ForeignKey(
+        to=Advertisement,
+        verbose_name='채용공고',
+        on_delete=models.CASCADE,
+        related_name='applications'
+    )
